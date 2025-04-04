4af22582b1d52382ce8b195675df7ee1747e08f3
Petr <petaschlup@seznam.cz>
2025-04-04 22:49:53 +0200

    initial commit

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="ct_detector",
     version="0.1.0",
-    packages=find_packages(),
+    packages=["ct_detector"],
     install_requires=[
         "ultralytics",
         "numpy",
