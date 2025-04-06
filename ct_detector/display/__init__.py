84ff5b4402b30f664287034320bd2633b2a7b1ba
Petr <petaschlup@seznam.cz>
2025-04-06 02:56:17 +0200

    Improved plot display callback.

--- ct_detector/display/__init__.py
+++ ct_detector/display/__init__.py
@@ -1,3 +1,3 @@
-from ct_detector.display.plots import display_confusion_matrix
+from ct_detector.display.plots import display_file_image
 
-__all__ = [display_confusion_matrix]
+__all__ = [display_file_image]
--- ct_detector/display/plots.py
+++ ct_detector/display/plots.py
@@ -1,14 +1,14 @@
 from IPython.display import Image, display
 
-def display_confusion_matrix(plot_fname, width=None, height=None):
+def display_file_image(image_path, width=None, height=None):
     """
-    A callback function to display the confusion matrix plot in a Jupyter notebook.
+    A callback function to display an image in a Jupyter notebook.
     Use as the on_plot argument of the plot method of a <class 'ultralytics.utils.metrics.ConfusionMatrix'> instance.
     Display the saved image in the Jupyter notebook with custom size.
-
     Args:
-        plot_fname (str or Path): Path to the image file
+        image_path (str): Path to the image file
         width (int, optional): Width of the displayed image in pixels
         height (int, optional): Height of the displayed image in pixels
     """
     display(Image(filename=str(plot_fname), width=width, height=height))
+    display(Image(filename=str(image_path), width=width, height=height))
